ec86deea63cc5f07ef4f72d966baa153c7fca5b4
kamil180895 <kamil180895@wp.pl>
2023-06-13 16:56:19 +0200

    Working rocket

---
+++ Assets/RocketScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    public float directingForceMagnitude;
+    public float velocityMagnitude;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0f, velocityMagnitude);
+        Vector2 v = rb.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(players.Length);
+        GameObject nearestPlayer = players[0];
+        float minDist = (nearestPlayer.transform.position - transform.position).magnitude;
+        foreach(GameObject p in players)
+        {
+            float dist = (p.GetComponent<Transform>().position - transform.position).magnitude;
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearestPlayer = p;
+            }
+        }
+
+        Vector2 forceDirection = nearestPlayer.transform.position - transform.position;
+        rb.AddForce(forceDirection.normalized * directingForceMagnitude);
+        rb.velocity = rb.velocity.normalized * velocityMagnitude;
+
+        Vector2 v = rb.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+        //jeśli kolizja z graczem to resetuj poziom
+    }
+}
