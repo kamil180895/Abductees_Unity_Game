10316cc6263a7c03f3dfe643eb63d8cfc46e0eb9
kamil180895 <kamil180895@wp.pl>
2023-06-15 19:04:23 +0200

    Added end door

---
+++ Assets/EndDoorScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDoorScript : MonoBehaviour
+{
+    public float endingTriggerDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject farthestPlayer = players[0];
+        float maxDist = (farthestPlayer.transform.position - transform.position).magnitude;
+        foreach (GameObject p in players)
+        {
+            float dist = (p.GetComponent<Transform>().position - transform.position).magnitude;
+            if (dist > maxDist)
+            {
+                maxDist = dist;
+                farthestPlayer = p;
+            }
+        }
+
+        if(maxDist < endingTriggerDistance)
+        {
+            //zakończ poziom
+            Debug.Log("Wygrałeś");
+        }
+    }
+}
