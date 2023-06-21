15795984b9855357a53cc0ae257f64a0a5920fae
filipmaciejak <fimaciejak@gmail.com>
2023-06-22 01:31:45 +0200

    lever sprites

--- Assets/Lever/LeverScript.cs
+++ Assets/Lever/LeverScript.cs
@@ -7,11 +7,15 @@ public class LeverScript : MonoBehaviour
 
     public bool leverState { get; private set; }
     public float maxDistance;
+    public Sprite leverOn;
+    public Sprite leverOff;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         leverState = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,5 +27,13 @@ public class LeverScript : MonoBehaviour
     public void SwitchLever()
     {
         leverState = !leverState;
+        if (leverState)
+        {
+            spriteRenderer.sprite = leverOn;
+        }
+        else
+        {
+            spriteRenderer.sprite = leverOff;
+        }
     }
 }
