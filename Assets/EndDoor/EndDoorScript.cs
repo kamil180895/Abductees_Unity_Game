46b43990915ddcbc8ff3fd4eec006e2a8a708551
filipmaciejak <fimaciejak@gmail.com>
2023-06-22 13:03:48 +0200

    scene change 1 -> 2

--- Assets/EndDoor/EndDoorScript.cs
+++ Assets/EndDoor/EndDoorScript.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndDoorScript : MonoBehaviour
 {
     public float endingTriggerDistance;
+    public string sceneName;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
@@ -30,7 +30,7 @@ public class EndDoorScript : MonoBehaviour
 
         if(maxDist < endingTriggerDistance)
         {
-            //zakończ poziom
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
             Debug.Log("Wygrałeś");
         }
     }
