cc6d77f30a6c8963ac08d5a04f8bbbda501d1d56
filipmaciejak <fimaciejak@gmail.com>
2023-06-22 10:57:25 +0200

    lever sound

--- Assets/Lever/LeverScript.cs
+++ Assets/Lever/LeverScript.cs
@@ -10,12 +10,14 @@ public class LeverScript : MonoBehaviour
     public Sprite leverOn;
     public Sprite leverOff;
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         leverState = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,5 +37,6 @@ public class LeverScript : MonoBehaviour
         {
             spriteRenderer.sprite = leverOff;
         }
+        audioSource.Play();
     }
 }
